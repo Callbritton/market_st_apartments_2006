726a2642fcad29edf7704b2e1cea80409c56be2d
Chris Allbritton <cla.inbox@gmail.com>
2020-07-14 11:18:02 -0400

    test_it_has_bedrooms pass

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -32,4 +32,18 @@ class ApartmentTest < Minitest::Test
     assert_equal 1, unit1.bathrooms
   end
 
+  def test_it_has_monthly_rent
+
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal 1200, unit1.monthly_rent
+  end
+
+  def test_it_has_bedrooms
+
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal 1, unit1.bedrooms
+  end
+
 end
