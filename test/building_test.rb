7cc755e8f47276b90bdfe43d903483e5ce218d0c
Chris Allbritton <cla.inbox@gmail.com>
2020-07-14 11:36:31 -0400

    Add test_renters_start_empty

--- test/building_test.rb
+++ test/building_test.rb
@@ -31,5 +31,12 @@ class BuildingTest < Minitest::Test
 
     assert_equal [unit1, unit2], building.units
   end
-  
+
+  def test_renters_start_empty
+
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    assert_equal [], building.renters
 end
